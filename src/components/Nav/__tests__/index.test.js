f822c7f82ef92d6d65de4301fc84633b2e3f6db0
Rush <kriscookies20@gmail.com>
2022-07-21 21:59:47 -0700

    finished testing and fixed errors

---
+++ src/components/Nav/__tests__/index.test.js
@@ -0,0 +1,47 @@
+//impoting packages needed to test the code
+import React from 'react';
+import { render, cleanup } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Nav from '..';
+
+// configure testing by adding cleanup utlity & the describe function
+
+    // adding the cleanup statement
+    afterEach(cleanup);
+
+    // add describe function to declare what this test suite will be testing
+    describe('Nav component', () => {
+        // baseline test
+        it('renders', () => {
+          render(<Nav />);
+        });
+
+     // snapshot test
+  it('matches snapshot', () => {
+    const { asFragment } = render(<Nav />);
+    // assert value comparison
+    expect(asFragment()).toMatchSnapshot();
+  });
+})
+
+//testing if camera emoji is visible
+describe('emoji is visible', () => {
+    it('inserts emoji into the h2', () => {
+    // Arrange
+        const { getByLabelText } = render(<Nav />);
+    // Assert
+        expect(getByLabelText('camera')).toHaveTextContent('📸');
+    });
+  })
+
+  // testing if some links are visible
+  describe('links are visible', () => {
+    it('inserts text into the links', () => {
+      // Arrange
+      const { getByTestId } = render(<Nav />);
+      // Assert
+      expect(getByTestId('link')).toHaveTextContent('Oh Snap!');
+      expect(getByTestId('about')).toHaveTextContent('About me');
+    });
+  })
+  
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -23,26 +23,29 @@ function Nav() {
   return (
     <header>
         <h2>
-    <a href="/">
+    <a data-testid="link" href="/">
       <span role="img" aria-label="camera"> 📸</span> Oh Snap!
     </a>
   </h2>
   <nav>
     <ul className="flex-row">
       <li className="mx-2">
-        <a href="#about">
+        <a data-testid="about" href="#about">
           About me
         </a>
       </li>
       <li>
         <span>Contact</span>
       </li>
+      {/* categories.map goes through the catagories array & it's naming the name and description category */}
+       {/* key is grabbing category.name-->defining attribute (need to put a key when you put a dot map) */}
       {categories.map((category) => (
         <li
             className="mx-1"
-            key={category.name}
-            >
-                <span onClick={categorySelected(category.name)} >
+            key={category.name}>
+
+                 {/* grabbing the name b/c category is an object (dot notation) */}
+                <span onClick={() => categorySelected(category.name)} >
                     {category.name}
                     </span>
             </li>
