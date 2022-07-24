09299763b3c8b647474daab741f893f6abaa811f
Rush <kriscookies20@gmail.com>
2022-07-24 06:48:05 -0700

    tested the modal

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -107,14 +107,14 @@ const PhotoList = ({ category }) => {
       const toggleModal = (image, i) => {
         // current photo
         setCurrentPhoto({...image, index: i})
-        setIsModalOpen(true);
+        setIsModalOpen(!isModalOpen);
       }
 
         const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <div>
-      {isModalOpen && <Modal currentPhoto={currentPhoto} />}
+      {isModalOpen && <Modal currentPhoto={currentPhoto} onClose={toggleModal} />}
 
         <div className='flex-row'>
             {currentPhotos.map((image, i) => (
